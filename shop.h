b59508e3354cd23a8619cea578a0995066861e11
huyentran2008 <huyentran208207@gmail.com>
2026-05-08 14:28:44 +0700

    Update shop.h

--- shop.h
+++ shop.h
@@ -18,6 +18,16 @@
 #define MAX_SP 100
 #define MAX_NV 100
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define BOLD    "\033[1m"
+
 // 1. Module Chic Flow (San pham thiet ke)
 typedef struct Product {
     int id;
