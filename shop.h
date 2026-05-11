bad75d2dfc6a16265fb27b939818eb919cdf0fe2
nguyentantai3333777-maker <nguyentantai3333777@gmail.com>
2026-05-11 13:55:41 +0700

    Update shop.h

--- shop.h
+++ shop.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 #include <windows.h>
+#include <time.h>
 
 #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
 #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
@@ -14,47 +14,49 @@
 #define MAX_STR 100
 #define FILE_NAME "chic_flow_data.txt"
 #define FILE_THUCHI "thuchi.csv"
+#define FILE_NHANVIEN "nhanvien.txt"
+#define FILE_KHOHANG "khohang.txt"
 #define MAX_SP 100
 #define MAX_NV 100
-
-// 1. Module Chic Flow (San pham thiet ke)
 typedef struct Product {
     int id;
     char name[MAX_STR];
     float price;
+    int soLuong;  
+    float giamgia; 
+    int daban;
     struct Product *next;
 } Product;
 
-// 2. Module Thu Chi
 typedef struct {
     char ngay[20];
     char moTa[100];
     float soTien;
-    int loai; // 1: Thu, 0: Chi
+    int loai;
 } ThuChi;
 
-// 3. Module Nhan Vien
 typedef struct {
     char hoTen[50];
     int tuoi;
     char gioiTinh[20];
+    char chucVu[30];
     float heSoluong; 
 } NhanVien;
 
-// 4. Module Kho Hang San Pham
 typedef struct {
-   char Tensp[100];
-   char Size[10];
-   int soLuong;
-   float Gia;
-   int daban;
-   float giamgia;
+	int id; 
+ 	char Tensp[100];
+   	char Size[10];
+   	int soLuong;
+   	float Gia;
+   	int daban;
+   	float giamgia;
 } Sanpham;
 
-// Tien ich UI & He thong
+// --- Prototypes ---
 void clearBuffer();
 void setConsoleWindow();
-int getKeyboardChoice(int numOptions); // Ð? ð?i tên và tham s?
+int getKeyboardChoice(int numOptions); 
 void printHeader(const char* title);
 void pauseConsole();
 
@@ -62,32 +64,37 @@ void pauseConsole();
 void ghiGiaoDich(ThuChi tc);
 void themGiaoDich();
 void lapBaoCaoThuChi();
+void menuTaiChinh();
 
-// Module Chic Flow (Thiet ke)
-void addProduct(Product **head);
-void sellProductChicFlow(Product *head);
+// Module Chic Flow
+void importFromWarehouse(Product **head, int *nKho, Sanpham dsKho[]);
+void displayStoreProducts(Product *head);
+void sellAtStore(Product *head); 
+void updateProduct(Product *head);   
+void deleteProduct(Product **head);  
+void sortProducts(Product **head);   
 void saveToFile(Product *head);
 void loadFromFile(Product **head);
+void exportReport(Product *head);
 void freeList(Product *head);
-void menuChicFlow(Product **head);
+void favProductsChicFlow(Product *head);
+void menuChicFlow(Product **head, int *nKho, Sanpham dsKho[]);
 
 // Module Nhan Vien
+void saveNVToFile(int n, NhanVien a[]);
+void loadNVFromFile(int *n, NhanVien a[]);
 void nhapNV(int *n, NhanVien a[]);
 void xuatNV(int n, NhanVien a[]);
 void themNhanVien(int *n, NhanVien a[]);
-void xoaNhanVien(int *n, NhanVien a[]);
-float tongHeSoLuong(int n, NhanVien a[]);
-float trungBinhHeSoLuong(int n, NhanVien a[]);
+void xoaNV(int *n, NhanVien a[]);
+void tinhLuong(int n, NhanVien a[]);
 void menuNhanVien(int *n, NhanVien a[]);
 
 // Module Kho Hang
-int nhapSoLuong();
+void saveKhoToFile(int n, Sanpham ds[]);
+void loadKhoFromFile(int *n, Sanpham ds[]);
 void nhapHang(int *n, Sanpham ds[]);
 void showKho(int n, Sanpham ds[]);
-void discountKho(int n, Sanpham ds[]);
-void sellKho(int n, Sanpham ds[]);
-void kiemTraKho(int n, Sanpham ds[]);
-void favProducts(int n, Sanpham ds[]);
 void menuKhoHang(int *n, Sanpham ds[]);
 
 #endif
